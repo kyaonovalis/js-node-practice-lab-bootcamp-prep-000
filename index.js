a3b7a13995d5994599749232afc1e2f8e6c253a3
Novalis YAO <kyaonovalis@gmail.com>
2018-01-15 23:32:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
 var name = "Novalis"
-var height = "193"
+var height = 193
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+console.log(parseInt(height) + 1 )
